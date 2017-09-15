1a2399218abf9c565fe2cbeb81cc875f90033ada
Grzegorz Pawlak <kalejdoskopwroclaw@gmail.com>
2017-09-15 17:33:38 +0200

    add new file

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+function drawTree(n) {
+	for( i = 0; i <= n; i++) {
+		var star = '';
+		for ( j = 0; j < i; j++) {
+			var star = star += '*';
+		};
+		console.log(star);
+		}
+	}
+
+drawTree(7)
+
+
+
+
